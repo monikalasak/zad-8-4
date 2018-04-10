95fe5556171f4df6dacd1fb966e5b23f5bb3ffbe
monikalasak <37957212+monikalasak@users.noreply.github.com>
2018-04-10 17:53:31 +0200

    add scrips.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+// scripts.js
+
+var name = prompt('Enter your name');
+{
+/// Komentarz 1
+}
+alert('Hello, ' + name);
+{
+	//// komentaz 2
+}
+console.log('Hello, ' + name);
+{
+	/// Komentarz 3git 
+}
